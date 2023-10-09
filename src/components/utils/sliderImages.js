c9ebc099680bc15756be801a215f7395f9c51fcd
Stephen Chris <46078420+StephenChris91@users.noreply.github.com>
2023-10-09 01:00:38 +0100

    updated slider image links

--- src/components/utils/sliderImages.js
+++ src/components/utils/sliderImages.js
@@ -1,7 +1,7 @@
-import slide_image_1 from "../../assets/images/img_1.jpg";
-import slide_image_2 from "../../assets/images/img_2.jpg";
-import slide_image_3 from "../../assets/images/img_3.jpg";
-import slide_image_4 from "../../assets/images/img_4.jpg";
+import slide_image_1 from "/assets/images/img_1.png";
+import slide_image_2 from "/assets/images/img_2.png";
+import slide_image_3 from "/assets/images/img_3.png";
+import slide_image_4 from "/assets/images/img_4.png";
 
 export const sliderImages = [
   slide_image_1,
