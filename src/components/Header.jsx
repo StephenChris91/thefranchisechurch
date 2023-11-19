ce5b67fb03418181aced0657813aa3e345524b30
Stephen Chris <46078420+StephenChris91@users.noreply.github.com>
2023-11-20 00:51:59 +0100

    updated functionality for livestream code

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,5 @@
-// src/components/Header.js
-import React from 'react';
-// import './Header.scss';
-import { FaYoutube } from 'react-icons/fa'
+
+
 
 const Header = () => {
   return (
@@ -10,14 +8,6 @@ const Header = () => {
         <source src="assets/opener.mp4" type="video/mp4" />
         Your browser does not support the video tag.
       </video>
-      <div className="header-content">
-
-        {/* <h1>THIS IS HOME</h1> */}
-        {/* <button class="button-primary">
-          Join Us Today
-         <FaYoutube className='icon' />
-        </button> */}
-      </div>
     </header>
   );
 };
