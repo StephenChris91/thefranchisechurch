6709cfd734c87a5b294d79ec322c94db97efc556
Stephen Chris <46078420+StephenChris91@users.noreply.github.com>
2023-11-22 23:35:19 +0100

    updated navbar code to automatically remove mobile nav on route change

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,50 +1,53 @@
-import { useRef } from 'react'
+import { useRef, useEffect } from 'react';
+import { FaYoutube } from 'react-icons/fa';
+import { GiHamburgerMenu } from 'react-icons/gi';
+import { FaTimes } from 'react-icons/fa';
+import { Link, useLocation } from 'react-router-dom';
 
-import { FaYoutube } from 'react-icons/fa'
-import { GiHamburgerMenu } from 'react-icons/gi'
-import { FaTimes } from 'react-icons/fa'
-import { Link } from "react-router-dom"
-
-
-
-
-function Navbar( { color } ) {
-
+function Navbar({ color }) {
   const navRef = useRef();
+  const location = useLocation();
 
   const openMobileNav = () => {
-    navRef.current.classList.toggle('mobile-nav')
-    // console.log(navRef)
-  }
+    navRef.current.classList.toggle('mobile-nav');
+  };
 
-    return (
-      <div id="header-wrapper">
+  const closeMobileNav = () => {
+    navRef.current.classList.remove('mobile-nav');
+  };
+
+  useEffect(() => {
+    // Close mobile nav on route change
+    closeMobileNav();
+  }, [location.pathname]);
+
+  return (
+    <div id="header-wrapper">
       <header>
-            <Link to='/'>
-                <img src="/assets/images/logo.png" alt="logo" />
-            </Link>
-            <nav className="nav" ref={navRef}>
-                <Link to='/'>Home</Link>
-                <Link to='/about'>About</Link>
-                {/* <Link to='/event'>Events</Link> */}
-                <Link to='/livestream'>Livestream</Link>
-                <button className="">
-                    Join Us Online
-                    <FaYoutube className='icon'/>
-                </button>
-                <button  className='mobile-close-btn nav-btn' onClick={openMobileNav}>
-                    <FaTimes/>
-                </button>
-              </nav>
-            <button 
-              className="mobile-open-btn nav-btn" 
-              onClick={() => openMobileNav()}>
-                <GiHamburgerMenu />
-            </button>
+        <Link to="/">
+          <img src="/assets/images/logo.png" alt="logo" />
+        </Link>
+        <nav className="nav" ref={navRef}>
+          <Link to="/">Home</Link>
+          <Link to="/about">About</Link>
+          <Link to="/livestream">Livestream</Link>
+          <button className="">
+            Join Us Online
+            <FaYoutube className='icon'/>
+          </button>
+          <button className='mobile-close-btn nav-btn' onClick={closeMobileNav}>
+            <FaTimes />
+          </button>
+        </nav>
+        <button
+          className="mobile-open-btn nav-btn"
+          onClick={openMobileNav}
+        >
+          <GiHamburgerMenu />
+        </button>
       </header>
-      </div>
-    )
-  }
-  
-  export default Navbar
-  
+    </div>
+  );
+}
+
+export default Navbar;
